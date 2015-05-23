91a9a8d857f9083d176057f5ffdc764174f07960
Matthias Rank <mail@m-rank.de>
2015-05-23 20:28:36 +0200

    Initial commit

---
+++ iControlPluginLogWriter/iControlPluginLogWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using iControlPluginInterface;
+
+namespace iControlPluginLogWriter {
+    class iControlPlugin : IiControlPlugin {
+        public string Name {
+            get {
+                return "LogWriter";
+            }
+        }
+
+        public string Author {
+            get {
+                return "Matthias Rank";
+            }
+        }
+
+        private iControlPluginInterface.IiControlPluginHost pluginHost;
+        public iControlPluginInterface.IiControlPluginHost Host {
+            set {
+                pluginHost = value;
+            }
+            get {
+                return pluginHost;
+            }
+        }
+
+        public void Handle(string[] commands, string ip) {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\commandlog.txt";
+            string text = String.Format("[{0:s}] [{1,15}] >> {2}", System.DateTime.Now, ip, String.Join(" ", commands)) + Environment.NewLine;
+
+            System.IO.File.AppendAllText(path, text);
+        }
+    }
+}
